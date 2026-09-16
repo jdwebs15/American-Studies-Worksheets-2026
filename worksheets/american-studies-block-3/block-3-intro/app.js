560b3911403ecc1c0e1e2f9fbca250b0bc6f014a
jdwebs15 <dweber@nortonpanthers.org>
2026-09-16 09:11:47 -0400

    Update APPS_SCRIPT_URL to production URL

--- worksheets/american-studies-block-3/block-3-intro/app.js
+++ worksheets/american-studies-block-3/block-3-intro/app.js
@@ -1,4 +1,4 @@
-const APPS_SCRIPT_URL="PASTE_YOUR_APPS_SCRIPT_WEB_APP_URL_HERE";
+const APPS_SCRIPT_URL="https://script.google.com/macros/s/AKfycbxsemmwRlVONU7h3pU2Ou70T8GKkVPFY5syHSrZp7MpiQCTj7WtZvaurgdFZmtcGP6a/exec";
 const questions=window.BLOCK_QUESTIONS;
 const C=window.BLOCK_CONFIG;
 const $=id=>document.getElementById(id);
