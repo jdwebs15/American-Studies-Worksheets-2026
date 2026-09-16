5d6a66b3a7cd286b45bb7b6744e6b2d5619ccc0d
jdwebs15 <dweber@nortonpanthers.org>
2026-09-16 09:15:44 -0400

    Update APPS_SCRIPT_URL to the correct endpoint

--- worksheets/american-studies-block-3/block-3-vocabulary/app.js
+++ worksheets/american-studies-block-3/block-3-vocabulary/app.js
@@ -1,4 +1,4 @@
-const APPS_SCRIPT_URL="PASTE_YOUR_APPS_SCRIPT_WEB_APP_URL_HERE";
+const APPS_SCRIPT_URL="https://script.google.com/macros/s/AKfycbyh9TiBdK6lilT3we8elyAAx9-m-c0C5X1z8K9RA01eFD2IplI5EenejkmXcXJNJGoM/exec";
 const questions=window.BLOCK_QUESTIONS;
 const C=window.BLOCK_CONFIG;
 const $=id=>document.getElementById(id);
