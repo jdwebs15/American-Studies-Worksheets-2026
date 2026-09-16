117696243b49317bfb0af7ca2ad11b11469e2469
jdwebs15 <dweber@nortonpanthers.org>
2026-09-16 13:42:04 -0400

    Update app.js

--- worksheets/block-10-vocabulary/app.js
+++ worksheets/block-10-vocabulary/app.js
@@ -1,4 +1,4 @@
-const APPS_SCRIPT_URL="PASTE_YOUR_APPS_SCRIPT_WEB_APP_URL_HERE";
+const APPS_SCRIPT_URL="https://script.google.com/macros/s/AKfycbz_N40FGUxCgJBfpIUtR3CgC0nRfKFI1KdqalcKRtj_zK2rZcIIGReS9MEwDSODnsAn_Q/exec";
 const questions=window.BLOCK_QUESTIONS;
 const C=window.BLOCK_CONFIG;
 const $=id=>document.getElementById(id);
