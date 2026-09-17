003e52c48c93015efaa176f762c5a850fc856646
jdwebs15 <dweber@nortonpanthers.org>
2026-09-17 09:43:01 -0400

    Update APPS_SCRIPT_URL to the correct endpoint

--- worksheets/block-1-vocabulary/app.js
+++ worksheets/block-1-vocabulary/app.js
@@ -1,4 +1,4 @@
-const APPS_SCRIPT_URL="PASTE_YOUR_APPS_SCRIPT_WEB_APP_URL_HERE";
+const APPS_SCRIPT_URL="https://script.google.com/macros/s/AKfycbxOkyzvD3SgH2O2VH1I5NAGU8aWANuIiqZPlJGzj5QX90DZluNZ0T9dVSNag-P_w5t9Yw/exec";
 const questions=window.BLOCK_QUESTIONS;
 const C=window.BLOCK_CONFIG;
 const $=id=>document.getElementById(id);
